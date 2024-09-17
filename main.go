a25b1e4fde907d46c88dd96b19b3329d6990a2c4
Icosatess <icosatess@example.com>
2024-09-17 00:07:59 -0400

    Renders highlighted component

--- main.go
+++ main.go
@@ -1,12 +1,36 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"log"
 	"net/http"
 )
 
+var activeComponent string = "server"
+
+func updateActiveComponent(w http.ResponseWriter, r *http.Request) {
+	bs, bserr := io.ReadAll(r.Body)
+	if bserr != nil {
+		panic(bserr)
+	}
+	activeComponent = string(bs)
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	t, terr := template.ParseFiles(`C:\Users\Icosatess\Source\minimapui\index.html`)
+	if terr != nil {
+		panic(terr)
+	}
+	t.Execute(w, struct{ Component string }{
+		Component: "minimap-" + activeComponent,
+	})
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir(`C:\Users\Icosatess\Source\minimapui`)))
+	http.HandleFunc("POST /component/", updateActiveComponent)
+	http.Handle("/public/", http.FileServer(http.Dir(`C:\Users\Icosatess\Source\minimapui`)))
+	http.HandleFunc("/", root)
 
 	log.Printf("Starting static file server at 127.0.0.1:8080")
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
